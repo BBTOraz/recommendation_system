7e9b625645214d6fc8233ef23edc17b8dd36818d
BBT Yuta <orazik0902@gmail.com>
2024-11-12 19:16:27 +0500

    получение ключей через API

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI
 from app.routers import recommendations
 
@@ -11,3 +13,13 @@ app.include_router(recommendations.router)
 @app.get("/")
 def read_root():
     return {"message": "Добро пожаловать в Movie Recommendation API"}
+
+KINOPARK_API_TOKEN = os.getenv("KINOPARK_API_TOKEN")
+DEEPL_API_KEY = os.getenv("DEEPL_API_KEY")
+
+@app.get("/check-env")
+def check_env():
+    return {
+        "KINOPARK_API_TOKEN": KINOPARK_API_TOKEN,
+        "DEEPL_API_KEY": DEEPL_API_KEY
+    }
