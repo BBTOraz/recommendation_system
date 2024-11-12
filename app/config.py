afca943f8c2a785273e9dd77e3301ee0ac032932
BBT Yuta <orazik0902@gmail.com>
2024-11-12 19:03:35 +0500

    проверка на существования ключей

--- app/config.py
+++ app/config.py
@@ -7,4 +7,6 @@ load_dotenv()
 DEEPL_API_KEY = os.getenv('DEEPL_API_KEY')
 KINOPARK_API_TOKEN = os.getenv('KINOPARK_API_TOKEN')
 
+print(f"KINOPARK_API_TOKEN: {KINOPARK_API_TOKEN}")
+print(f"DEEPL_API_KEY: {DEEPL_API_KEY}")
 
