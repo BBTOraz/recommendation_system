c058a03e6c6a2792215a1ae81b0700e9bf9a149d
BBT Yuta <orazik0902@gmail.com>
2024-11-17 03:22:36 +0500

    уже не помню какой фикс и для чего

--- app/models/recommender.py
+++ app/models/recommender.py
@@ -1,5 +1,3 @@
-# app/models/recommender.py
-
 import pandas as pd
 import numpy as np
 import requests
@@ -7,12 +5,13 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from app.utils.translators import translate_text, translate_and_map_genres
 from app.config import KINOPARK_API_TOKEN
-from functools import lru_cache
 import datetime
-import threading
+import logging
 
 class MovieRecommender:
     def __init__(self):
+        self.kinopark_df = None
+        self.merged_df = None
         self.imdb_data_path = "data/imdb_movie_data.csv"
         self.df = None
         self.tfidf_vectorizer = None
@@ -20,23 +19,32 @@ class MovieRecommender:
         self.genre_mapping = {
             'cartoon': 'animation',
         }
-        self.load_imdb_data()
         self.user_history_normalized = None
-
+        self.load_imdb_data()
 
     def load_imdb_data(self):
-        print("Загрузка данных IMDb")
+        logging.info("Загрузка данных IMDb")
         self.df = pd.read_csv(self.imdb_data_path)
+
+        self.df = self.df[self.df['Year'] >= 2012]
+
+        self.df = self.df.sample(n=5000, random_state=42)
+
         self.df['source'] = 'imdb'
         text_columns = ['genres', 'directors', 'writers', 'actors']
         for col in text_columns:
             self.df[col] = self.df[col].fillna('')
         self.df['Movie'] = self.df['Movie'].fillna('Unknown')
         self.df['Year'] = self.df['Year'].fillna('Unknown')
-        self.df['content'] = self.df['genres'] + ' ' + self.df['directors'] + ' ' + self.df['writers'] + ' ' + self.df['actors']
+        self.df['content'] = (
+            self.df['genres'] + ' ' +
+            self.df['directors'] + ' ' +
+            self.df['writers'] + ' ' +
+            self.df['actors']
+        )
 
     def get_kinopark_data(self, city_id):
-        print("Получение текущей даты в формате YYYY-MM-DD")
+        logging.info("Получение текущей даты в формате YYYY-MM-DD")
         current_date = datetime.datetime.now().strftime("%Y-%m-%d")
         url = f"https://afisha.api.kinopark.kz/api/movie/today?city={city_id}&start={current_date}"
         headers = {
@@ -49,29 +57,25 @@ class MovieRecommender:
             if 'data' in kinopark_data and kinopark_data['data']:
                 self.kinopark_df = pd.json_normalize(kinopark_data['data'])
             else:
-                print("Нет данных о фильмах для указанного города.")
+                logging.warning("Нет данных о фильмах для указанного города.")
                 self.kinopark_df = pd.DataFrame()
         else:
-            print(f"Ошибка при получении данных из Kinopark API для города {city_id}")
+            logging.error(f"Ошибка при получении данных из Kinopark API для города {city_id}")
             self.kinopark_df = pd.DataFrame()
 
-    @lru_cache(maxsize=10000)
-    def cached_translate_text(self, text):
-        print("кеширование перевода текста")
+    def translate_text_no_cache(self, text):
         if not isinstance(text, str):
             text = str(text)
         return translate_text(text)
 
-    @lru_cache(maxsize=10000)
-    def cached_translate_and_map_genres(self, genres_tuple):
-        print("кеширование перевод и маппинг текста")
+    def translate_and_map_genres_no_cache(self, genres_tuple):
         return translate_and_map_genres(genres_tuple, self.genre_mapping)
 
     def process_kinopark_data(self):
-        print("Перевод и обработка данных Kinopark")
+        logging.info("Перевод и обработка данных Kinopark")
         if not self.kinopark_df.empty:
             self.kinopark_df['Year'] = pd.to_datetime(self.kinopark_df['release_date']).dt.year.astype(str)
-            self.kinopark_df['name_en'] = self.kinopark_df['name'].apply(lambda x: self.cached_translate_text(x))
+            self.kinopark_df['name_en'] = self.kinopark_df['name'].apply(lambda x: self.translate_text_no_cache(x))
             self.kinopark_df['genre_str'] = self.kinopark_df['genre'].apply(
                 lambda x: ', '.join(x) if isinstance(x, list) else ''
             )
@@ -80,38 +84,37 @@ class MovieRecommender:
             self.kinopark_df['imageHorizontal'] = self.kinopark_df['image.horizontal']
             self.kinopark_df['seanceTimeframes'] = self.kinopark_df['seance.timeframes']
             self.kinopark_df['genre_en'] = self.kinopark_df['genre'].apply(
-                lambda x: self.cached_translate_and_map_genres(tuple(x)) if isinstance(x, list) else ''
+                lambda x: self.translate_and_map_genres_no_cache(tuple(x)) if isinstance(x, list) else ''
             )
             self.kinopark_df['directors_str'] = self.kinopark_df['directors'].apply(
                 lambda x: ', '.join(x) if isinstance(x, list) else ''
             )
             self.kinopark_df['directors_en'] = self.kinopark_df['directors'].apply(
-                lambda x: ', '.join([self.cached_translate_text(director) for director in x]) if isinstance(x,
-                                                                                                            list) else ''
+                lambda x: ', '.join([self.translate_text_no_cache(director) for director in x]) if isinstance(x, list) else ''
             )
             self.kinopark_df['actors_str'] = self.kinopark_df['actors'].apply(
                 lambda x: ', '.join(x) if isinstance(x, list) else ''
             )
             self.kinopark_df['actors_en'] = self.kinopark_df['actors'].apply(
-                lambda x: ', '.join([self.cached_translate_text(actor) for actor in x]) if isinstance(x, list) else ''
+                lambda x: ', '.join([self.translate_text_no_cache(actor) for actor in x]) if isinstance(x, list) else ''
             )
             self.kinopark_df['Movie'] = self.kinopark_df['name_en'] + ' (' + self.kinopark_df['Year'] + ')'
             self.kinopark_df['content'] = (
-                    self.kinopark_df['genre_en'] + ' ' +
-                    self.kinopark_df['directors_en'] + ' ' +
-                    self.kinopark_df['actors_en']
+                self.kinopark_df['genre_en'] + ' ' +
+                self.kinopark_df['directors_en'] + ' ' +
+                self.kinopark_df['actors_en']
             )
             self.kinopark_df['source'] = 'kinopark'
         else:
-            print("Kinopark DataFrame is empty.")
+            logging.warning("Kinopark DataFrame is empty.")
 
     def merge_datasets(self):
-        print("Объединение IMDb и Kinopark данных")
+        logging.info("Объединение IMDb и Kinopark данных")
         necessary_columns = [
             'Movie', 'Year', 'content', 'source', 'id', 'name', 'trailer',
             'imageVertical', 'imageHorizontal', 'similarity'
         ]
-        print("Приведение столбцов к общему виду")
+        logging.info("Приведение столбцов к общему виду")
         for col in necessary_columns:
             if col not in self.df.columns:
                 self.df[col] = None
@@ -120,22 +123,26 @@ class MovieRecommender:
         self.merged_df = pd.concat([self.df, self.kinopark_df], ignore_index=True, sort=False)
 
     def prepare_tfidf(self):
-        print("Обучение TF-IDF модели на объединённых данных")
-        self.tfidf_vectorizer = TfidfVectorizer(stop_words='english', max_features=5000)
+        logging.info("Обучение TF-IDF модели на объединённых данных")
+        # **Оптимизация TF-IDF векторизатора**
+        self.tfidf_vectorizer = TfidfVectorizer(
+            stop_words='english',
+            max_features=2000,  # Уменьшаем количество признаков
+            max_df=0.8,         # Исключаем слишком частые слова
+            min_df=5            # Исключаем слишком редкие слова
+        )
         self.tfidf_matrix = self.tfidf_vectorizer.fit_transform(self.merged_df['content'].fillna(''))
 
     def prepare_kinopark_tfidf(self):
-        print("Преобразование контента Kinopark с помощью TF-IDF")
+        logging.info("Преобразование контента Kinopark с помощью TF-IDF")
         if not self.kinopark_df.empty:
             self.kinopark_tfidf = self.tfidf_vectorizer.transform(self.kinopark_df['content'].fillna(''))
         else:
             self.kinopark_tfidf = None
 
     def create_user_profile(self, user_history):
-        print("Нормализация названий фильмов из истории пользователя")
-        self.user_history_normalized = [self.cached_translate_text(title) for title in user_history]
-        """print("Нормализованные названия из истории пользователя:", self.user_history_normalized)
-        print("Доступные фильмы:", self.merged_df['Movie'].tolist())"""
+        logging.info("Нормализация названий фильмов из истории пользователя")
+        self.user_history_normalized = [self.translate_text_no_cache(title) for title in user_history]
         user_indices = self.merged_df[self.merged_df['Movie'].isin(self.user_history_normalized)].index
         if not user_indices.empty:
             user_tfidf = self.tfidf_matrix[user_indices]
@@ -143,18 +150,18 @@ class MovieRecommender:
             user_profile = np.asarray(user_profile)
             return user_profile
         else:
-            print("Фильмы из истории пользователя не найдены в объединенном датасете.")
+            logging.warning("Фильмы из истории пользователя не найдены в объединенном датасете.")
             return None
 
     def recommended_movies(self, user_profile, top_n=10):
-        print("Рекомендация фильмов пользователю")
+        logging.info("Рекомендация фильмов пользователю")
         if user_profile is not None and self.kinopark_tfidf is not None:
             similarities = cosine_similarity(user_profile, self.kinopark_tfidf).flatten()
             recommendations = self.kinopark_df.copy()
             recommendations['similarity'] = similarities
             recommendations = recommendations[~recommendations['Movie'].isin(self.user_history_normalized)]
             top_recommendations = recommendations.sort_values(by='similarity', ascending=False).head(top_n)
-            print("Рекомендация фильмов пользователю завершена")
+            logging.info("Рекомендация фильмов пользователю завершена")
             result_columns = [
                 'id',
                 'name',
@@ -165,11 +172,11 @@ class MovieRecommender:
             ]
             missing_columns = [col for col in result_columns if col not in top_recommendations.columns]
             if missing_columns:
-                print(f"Отсутствуют следующие столбцы: {missing_columns}")
+                logging.warning(f"Отсутствуют следующие столбцы: {missing_columns}")
                 for col in missing_columns:
                     top_recommendations[col] = None
             # Возвращаем только нужные столбцы
             return top_recommendations[result_columns]
         else:
-            print("Не удалось создать профиль пользователя или нет данных Kinopark.")
+            logging.warning("Не удалось создать профиль пользователя или нет данных Kinopark.")
             return None
