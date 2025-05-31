1f4d8b9f444f58172ff0eaef2c83875188182e4e
BBT Yuta <orazik0902@gmail.com>
2025-06-01 02:45:01 +0500

    фикс ошибки и логирование ответов

--- app/config.py
+++ app/config.py
@@ -1,16 +1,12 @@
 import os
-
-# Если контейнер/среда (Railway) уже установил эти переменные, os.getenv их подхватит.
-# Если вы локально держите .env, можно вызвать load_dotenv() ДО этих строк,
-# чтобы локально .env прочитался и попал в os.environ.
-
 from dotenv import load_dotenv
-load_dotenv()  # Откроет файл .env в корне проекта (только локально)
 
-KINOPARK_API_TOKEN = os.getenv("KINOPARK_API_TOKEN", "")
-DEEPL_API_KEY      = os.getenv("DEEPL_API_KEY", "")
+load_dotenv()
 
-if not KINOPARK_API_TOKEN:
-    raise RuntimeError("Не задана переменная окружения KINOPARK_API_TOKEN")
-if not DEEPL_API_KEY:
-    raise RuntimeError("Не задана переменная окружения DEEPL_API_KEY")
+
+DEEPL_API_KEY = os.getenv('DEEPL_API_KEY')
+KINOPARK_API_TOKEN = os.getenv('KINOPARK_API_TOKEN')
+
+print(f"KINOPARK_API_TOKEN: {KINOPARK_API_TOKEN}")
+print(f"DEEPL_API_KEY: {DEEPL_API_KEY}")
+
